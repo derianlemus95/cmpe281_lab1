b1a29fe2600c11e0d9454067043701b6b1d0e364
derianlemus95 <derian.lemus95@gmail.com>
2023-03-29 17:32:27 -0700

    Modified message

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>CMPE281</h1>
-      <h3>LAB 1</h3>
+      <h3>LAB 2</h3>
       <h5>Student :Derian Lemus</h5>
       <p>
         This lab assignment is designed to enable students to gain some hands-on
         experience cloud technology by running a simple app (such as this one
-        this one) on Amazon's EC2.
+        this one) on Google's App Engine
       </p>
     </div>
   );
